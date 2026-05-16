d31cb632c26b57ad9032dcdb02ead2f440d0527f
Rafy Hossain <rafyhossain@gmail.com>
2026-05-16 12:34:43 +0600

    Covid mission stages 1-3: intro sequence + 60 background + 32 party variants

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
      - Bump CACHE_VERSION whenever you ship breaking content/code
        changes; the old cache is purged on activate.
    ========================================================== */
-const CACHE_VERSION = 'goddi-v2-2026-05-15';
+const CACHE_VERSION = 'goddi-v3-2026-05-16';
 const SHELL = [
   '/',
   '/index.html',
